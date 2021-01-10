7056a5cdf03f39d8d366cf52e8b95a91522269b7
André Simões <alsimoes@users.noreply.github.com>
2021-01-10 18:10:31 -0300

    The game still broken

--- gdquest-2d-platformer/src/Actors/NewEnemie.gd
+++ gdquest-2d-platformer/src/Actors/NewEnemie.gd
@@ -8,19 +8,15 @@ func _ready() -> void:
 func _on_StompDetector_body_entered(body: Node) -> void:
 	if body.global_position.y > get_node("StompDetector").global_position.y:
 		return
-	get_node("CollisionShape2D").disabled = true
+	get_node("NewEnemieCollisionShape2D").disabled = true
 	queue_free()
 
 
 func _physics_process(delta: float) -> void:
 	_velocity.y += gravity * delta
-	if is_on_wall():
+	if  is_on_wall():
 		_velocity.x *= -1.0
 	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
-	
-	
-
-
 
 
 
