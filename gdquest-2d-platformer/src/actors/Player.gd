8a16f9deb507d850beb9096dd02cbca580b5f001
André Simões <alsimoes@users.noreply.github.com>
2021-01-10 09:57:41 -0300

    Equalizando com o codigo desenvolvido em outra máquina.

--- gdquest-2d-platformer/src/actors/Player.gd
+++ gdquest-2d-platformer/src/actors/Player.gd
@@ -1,32 +1,49 @@
 extends Actor
 
-# velocity.y = min(speed.y, velocity.y)
+export var stomp_impulse:= 1000.0
 
-func _physics_process(delta) -> void:
-	var is_jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0.0
+func _on_StompDetector_area_entered(area: Area2D) -> void:
+	_velocity = calculate_stomp_velocity(_velocity, stomp_impulse)
+
+
+func _on_EnemieDetector_body_entered(body: Node) -> void:
+	queue_free()
+
+
+func _physics_process(delta: float) -> void:
+	var is_jump_interrupted: = Input.is_action_just_released("jump") and _velocity.y < 0.0
 	var direction: = get_direction()
-	velocity = calculate_move_velocity(velocity, direction, speed, is_jump_interrupted)
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
-func get_direction() -> Vector2:
-	return Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
-		)
+	_velocity = calculate_move_velocity(_velocity, direction, speed, is_jump_interrupted)
+	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
 
-func calculate_move_velocity(
-	linear_velocity: Vector2,
-	direction: Vector2,
-	speed: Vector2,
-	is_jump_interrupted: bool) -> Vector2:
-	var output: Vector2 = linear_velocity
+
+func get_direction() -> Vector2:
+	return Vector2 (
+		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+		-1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 0.0
+	)
+
+
+func calculate_move_velocity(liner_velocity: Vector2, 
+							direction: Vector2, 
+							speed: Vector2, 
+							is_jump_interrupted: bool) -> Vector2:
+	var output: = liner_velocity
 	output.x = speed.x * direction.x
 	output.y += gravity * get_physics_process_delta_time()
-	
 	if direction.y == -1.0:
 		output.y = speed.y * direction.y
-		
 	if is_jump_interrupted:
 		output.y = 0.0
-	
 	return output
+
+
+func calculate_stomp_velocity(_velocity: Vector2, impulse: float) -> Vector2:
+	var output: = _velocity
+	output.y = -impulse
+	return output
+	
+
+
+
+
--- gdquest-2d-platformer/src/actors/actor.gd
+++ gdquest-2d-platformer/src/actors/actor.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
-
 class_name Actor
 
-export var speed: Vector2 = Vector2(300.0, 1000.0)
-export var gravity: float = 3500.0
-var velocity: Vector2  = Vector2.ZERO
+const FLOOR_NORMAL: = Vector2.UP
 
+export var speed: = Vector2(300.0, 1000.0)
+export var gravity: = 3500.0
+
+var _velocity: = Vector2.ZERO
